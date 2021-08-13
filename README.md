089af10a3ecc75303ee28494023d2ee2c7b1c4b0
mlopeland <mlopeland@protonmail.com>
2021-08-13 10:22:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### Development
+
+```
+docker-compose up -d
+docker-compose exec rust sh
+```
+
+### Links
+
+- https://riptutorial.com/rust/example/4404/a-simple-tcp-client-and-server-application--echo
+- https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.set_nonblocking
+- https://rust-lang-nursery.github.io/rust-cookbook/datetime/parse.html#display-formatted-date-and-time
+- https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::net::{ TcpListener, TcpStream };
+use std::{ thread, time, str, env };
+use std::io::{ Write, Read };
+use chrono::Utc;
+
+fn client_thread(target : String) {
+  loop {
+    match TcpStream::connect(format!("{}", target)) {
+      Ok(mut stream) => {
+        let msg = Utc::now().to_rfc3339();
+        stream.write(msg.as_bytes()).unwrap();
+        break;
+      },
+      Err(err) => {
+        println!("Client failed to connect: {}", err);
+      }
+    }
+    thread::sleep(time::Duration::from_millis(2000));
+  }
+}
+
+fn server_thread(local : String) {
+  let listener = TcpListener::bind(local).unwrap();
+  for stream in listener.incoming() {
+    match stream {
+      Ok(mut stream) => {
+        let mut data = [ 0 as u8 ; 50 ];
+        match stream.read(&mut data) {
+          Ok(_) => {
+            match str::from_utf8(&data) {
+              Ok(res) => {
+                println!("Time received: {}", res);
+                break;
+              },
+              Err(err) => {
+                println!("{}", err);
+              }
+            }
+          },
+          Err(err) => {
+            println!("Server unable to read message from client: {}", err);
+          }
+        }
+      },
+      Err(err) => {
+        println!("Server has failed to stablish connection: {}", err);
+      }
+    }
+  }
+}
+
+fn main() {
+  let args : Vec<String> = env::args().collect();
+  let target = format!("{}", args[1]);
+  let local = format!("{}", args[2]);
+
+  let server = thread::spawn(|| {
+    server_thread(local);
+  });  
+
+  let client = thread::spawn(|| {
+    client_thread(target);
+  });
+
+  server.join().unwrap();
+  client.join().unwrap();
+}
